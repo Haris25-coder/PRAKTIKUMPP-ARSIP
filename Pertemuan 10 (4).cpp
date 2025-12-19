0192d6c18ac6891342dd20b806ba2be74fed2682
HarisBrl25 <koringgo123@gmail.com>
2025-12-20 02:04:31 +0700

    Fungsi System

--- Pertemuan 10 (4).cpp
+++ Pertemuan 10 (4).cpp
@@ -1,72 +1,31 @@
 #include <ncurses\curses.h>
-
-void print_menu(WINDOW *menu_win, int pilihanTerpilih, const char *opsi[], int jumlahOpsi);
+#include <windows.h>
+using namespace std;
 
 int main(){
+    system("color C5");
     initscr();
-    clear();
-    noecho();
-    cbreak();
-    curs_set(0);
-    int awalX = 0, awalY = 0;
-    int lebar = 20, tinggi = 6;
 
-    WINDOW *menu_win = newwin(tinggi, lebar, awalY, awalX);
-    keypad(menu_win, TRUE);
-
-    const char *opsi[] = {
-        "Pilihan 1",
-        "Pilihan 2",
-        "Pilihan 3",
-        "Keluar"
-    };
-
-    int jumlahOpsi = sizeof(opsi) / sizeof(opsi[0]);
-    int pilihanTerpilih = 0;
-    int pilihan = -1;
-
-    while (1){
-        print_menu(menu_win, pilihanTerpilih, opsi, jumlahOpsi);
-        int tombol = wgetch(menu_win);
-        switch(tombol){
-            case KEY_UP:
-                if (pilihanTerpilih <= 0)
-                    pilihanTerpilih = jumlahOpsi - 1;
-                else
-                    --pilihanTerpilih;
-                break;
-            case KEY_DOWN:
-                if (pilihanTerpilih >= jumlahOpsi - 1)
-                    pilihanTerpilih = 0;
-                else
-                    ++pilihanTerpilih;
-                break;
-            case 10:
-                pilihan = pilihanTerpilih;
-                break;
-            default:
-                break;
-        }
-        if (pilihan == 3)
-            break;
-    }
-
-    clrtoeol();
+    mvprintw(2,2,"A");
     refresh();
+    Sleep(500);
+
+    mvprintw(3,2,"N");
+    refresh();
+    Sleep(1000);
+
+    mvprintw(4,2,"I");
+    refresh();
+    Sleep(1500);
+
+    mvprintw(5,2,"S");
+    refresh();
+    Sleep(500);
+
+    mvprintw(6,2,"A");
+    refresh();
+
+    getch();
     endwin();
     return 0;
-}
-
-void print_menu(WINDOW *menu_win, int pilihanTerpilih, const char *opsi[], int jumlahOpsi){
-    box(menu_win, 0, 0);
-    for (int i = 0; i < jumlahOpsi; ++i){
-        if (i == pilihanTerpilih){
-            wattron(menu_win, A_REVERSE);
-            mvwprintw(menu_win, i + 1, 1, "%s", opsi[i]);
-            wattroff(menu_win, A_REVERSE);
-        } else {
-            mvwprintw(menu_win, i + 1, 1, "%s", opsi[i]);
-        }
-    }
-    wrefresh(menu_win);
 }
